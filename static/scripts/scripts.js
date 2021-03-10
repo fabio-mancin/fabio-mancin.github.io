59ed536b8b7255a64ae3de865ddb49c68f7ce1e9
fabio-mancin <fabio.mancin90@gmail.com>
2021-03-10 09:59:59 +0100

    Accessibility

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     const t = $(this)[0];
     const logo = $(t.children[0])
     const bar = $(t.children[1])
-    if (logo.width() > "64.99999") {
+    if (logo.width() > "44.9999") {
       logo.animate({width: "-=15px", height: "-=15px"}, 500)
       bar.removeClass("hidden")
       bar.animate({
